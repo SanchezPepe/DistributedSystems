70b80bb76fa2353cc470278f8badf12d78a6de1a
Pepe Sánchez <SanchezPepe@users.noreply.github.com>
2020-02-12 19:02:14 -0600

    Add files via upload

---
+++ JavaTCPSocketsObj/src/javatcpsockets/TCPServer.java
@@ -0,0 +1,76 @@
+package javatcpsockets;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JGUTIERRGARC
+ */
+
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class TCPServer {
+    
+    public static void main (String args[]) {
+	try{
+		int serverPort = 7896; 
+		ServerSocket listenSocket = new ServerSocket(serverPort);
+		while(true) {
+			System.out.println("Waiting for messages..."); 
+                        Socket clientSocket = listenSocket.accept();  // Listens for a connection to be made to this socket and accepts it. The method blocks until a connection is made. 
+			Connection c = new Connection(clientSocket);
+                        c.start();
+		}
+	} catch(IOException e) {System.out.println("Listen :"+ e.getMessage());}
+    }
+   
+}
+
+class Connection extends Thread {
+	DataInputStream in;
+	DataOutputStream out;
+	Socket clientSocket;
+	public Connection (Socket aClientSocket) {
+	    try {
+		clientSocket = aClientSocket;
+		in = new DataInputStream(clientSocket.getInputStream());
+		out =new DataOutputStream(clientSocket.getOutputStream());
+	     } catch(IOException e)  {System.out.println("Connection:"+e.getMessage());}
+	}
+        
+        @Override
+	public void run(){
+	    try {	
+                ObjectInputStream objectInputStream =
+                    new ObjectInputStream(new FileInputStream("temp.tmp"));
+
+                Person personRead = (Person) objectInputStream.readObject();
+
+                objectInputStream.close();
+                System.out.println("Recibí el objeto: " + personRead.name);
+	    } 
+            catch(EOFException e) {
+                System.out.println("EOF:"+e.getMessage());
+	    } 
+            catch(IOException e) {
+                System.out.println("IO:"+e.getMessage());
+	    } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    clientSocket.close();
+                } catch (IOException e){
+                    System.out.println(e);
+                }
+            }
+        }
+}
+
+

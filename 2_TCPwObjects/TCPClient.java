a38fc360679c5c46dee072740933ebfa16ab6c2e
sanchezpepe <sanchezpepe97@gmail.com>
2020-02-12 23:09:46 -0600

    sent serialized java objects using TCPSockets

--- 2_TCPwObjects/TCPClient.java
+++ 2_TCPwObjects/TCPClient.java
@@ -2,8 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package javatcpsockets;
-
 import java.net.*;
 import java.io.*;
 
@@ -14,14 +12,7 @@ public class TCPClient {
 	Socket s = null;
 	    try {
 	    	int serverPort = 7896;
-	   	
                 s = new Socket("localhost", serverPort);    
-                //s = new Socket("127.0.0.1", serverPort);    
-		/** DataInputStream in = new DataInputStream( s.getInputStream());
-		DataOutputStream out =
-			new DataOutputStream( s.getOutputStream());
-		out.writeUTF("Hello");        	// UTF is a string encoding 
-                **/
                 FileOutputStream fos = new FileOutputStream("temp.tmp");
                 ObjectOutputStream oos = new ObjectOutputStream(fos);
                 

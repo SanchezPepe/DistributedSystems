f7051c13cfbceca72b9fdde4f5b6f54e765057dd
sanchezpepe <sanchezpepe97@gmail.com>
2020-02-17 22:25:07 -0600

    rmi example

---
+++ 3_RemoteInvocation/client/ComputeClient.java
@@ -0,0 +1,34 @@
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Logger;
+import interfaces.Compute;
+
+/**
+ * ComputeClient
+ */
+public class ComputeClient {
+
+    public static void main(String[] args) {
+        System.setProperty("java.security.policy", "3_RemoteInvocation/client/client.policy");
+
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+            String name = "Compute";
+            Registry registry = LocateRegistry.getRegistry("localhost"); // server's ip address
+            Compute comp = (Compute) registry.lookup(name);
+
+            System.out.println("3^2 = " + comp.square(3));
+            System.out.println("3^3 = " + comp.power(3,3));
+
+        } catch (RemoteException e) {
+            Logger.getLogger(e.toString());
+        } catch (NotBoundException e){
+            Logger.getLogger(e.toString());
+        }
+    }
+}
---
+++ 3_RemoteInvocation/server/ComputeServer.java
@@ -0,0 +1,47 @@
+import interfaces.Compute;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.logging.Logger;
+
+
+/**
+ * ComputeServer
+ */
+public class ComputeServer implements Compute {
+
+    public ComputeServer() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public double square(int number) throws RemoteException {
+        return (number * number);
+    }
+
+    @Override
+    public double power(int num1, int num2) throws RemoteException {
+        return (java.lang.Math.pow(num1, num2));
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.setProperty("java.security.policy", "3_RemoteInvocation/server/server.policy");
+
+            if (System.getSecurityManager() == null) {
+                System.setSecurityManager(new SecurityManager());
+            }
+            LocateRegistry.createRegistry(1099);
+
+            String name = "Compute";
+            ComputeServer engine = new ComputeServer();
+            Compute stub = (Compute) UnicastRemoteObject.exportObject(engine, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(name, stub);
+
+        } catch (RemoteException e) {
+            Logger.getLogger(e.toString());
+        }
+    }
+}
